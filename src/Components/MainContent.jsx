ef40b269dbd0596dee0d93e4b123fd76e1776f9f
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-03 23:46:22 -0500

    Fixed whitespace

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -74,7 +74,7 @@ export default function MainContent() {
       <section>
         <img src="hades-red.png" className="mx-auto" />
       </section>
-      <section className="w-full flex justify-center pt-5">
+      <section className="w-full flex justify-center pb-5">
         <Card className="w-[100%] backdrop-blur-lg bg-transparent shadow-[0_0_50px_cyan] 2xl:w-3/4">
           <img
             src="vertical-blue.jpg"
