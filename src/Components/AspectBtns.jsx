c3da1a2791b9b0d90e2b64612f81d4b74b6f773e
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-02-08 17:56:49 -0500

    Enforce similar layout

--- src/Components/AspectBtns.jsx
+++ src/Components/AspectBtns.jsx
@@ -1,51 +1,100 @@
 import { Button } from "@material-tailwind/react";
 
 const AspectButtons = ({ onButtonClick }) => {
-  const aspects = [
-    "Zagreus Sword",
-    "Nemesis",
-    "Poseidon",
-    "Arthur",
-    "Zagreus Spear",
-    "Achilles",
-    "Hades",
-    "Guan Yu",
-    "Zagreus Shield",
-    "Chaos",
-    "Zeus",
-    "Beowulf",
-    "Zagreus Bow",
-    "Chiron",
-    "Hera",
-    "Rama",
-    "Zagreus Fists",
-    "Talos",
-    "Demeter",
-    "Gilgamesh",
-    "Zagreus Rail",
-    "Eris",
-    "Hestia",
-    "Lucifer",
-  ];
-  const buttonNumbers = [
-    5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    25, 26, 27, 28,
-  ];
-  const buttonColors = "amber";
+  const aspects = ["Zagreus Sword", "Nemesis", "Poseidon", "Arthur"];
+  const aspects1 = ["Zagreus Spear", "Achilles", "Hades", "Guan Yu"];
+  const aspects2 = ["Zagreus Shield", "Chaos", "Zeus", "Beowulf"];
+  const aspects3 = ["Zagreus Bow", "Chiron", "Hera", "Rama"];
+  const aspects4 = ["Zagreus Fists", "Talos", "Demeter", "Gilgamesh"];
+  const aspects5 = ["Zagreus Rail", "Eris", "Hestia", "Lucifer"];
+
+  const buttonNumbers = [5, 6, 7, 8];
+  const buttonNumbers1 = [9, 10, 11, 12];
+  const buttonNumbers2 = [13, 14, 15, 16];
+  const buttonNumbers3 = [17, 18, 19, 20];
+  const buttonNumbers4 = [21, 22, 23, 24];
+  const buttonNumbers5 = [25, 26, 27, 28];
 
   return (
-    <div className="grid grid-cols-4 gap-2 pt-4">
-      {buttonNumbers.map((num, index) => (
-        <Button
-          key={index}
-          variant={onButtonClick(num).variant}
-          onClick={onButtonClick(num).onClick}
-          color={buttonColors}
-          className="text-center w-full lg:w-1/2 mx-auto py-4 px-2"
-        >
-          {`${aspects[index]}`}
-        </Button>
-      ))}
+    <div className="grid grid-cols-3 md:grid-cols-6">
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="blue"
+            className="w-[120px] p-3"
+          >
+            {`${aspects[index]}`}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers1.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="amber"
+            className="w-[120px] p-3"
+          >
+            {`${aspects1[index]}`}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers2.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="lime"
+            className="w-[120px] p-3"
+          >
+            {`${aspects2[index]}`}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers3.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="red"
+            className="w-[120px] p-3"
+          >
+            {`${aspects3[index]}`}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers4.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="light-blue"
+            className="w-[120px] p-3"
+          >
+            {`${aspects4[index]}`}
+          </Button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 place-items-center gap-2 p-2">
+        {buttonNumbers5.map((num, index) => (
+          <Button
+            key={index}
+            variant={onButtonClick(num).variant}
+            onClick={onButtonClick(num).onClick}
+            color="teal"
+            className="w-[120px] p-3"
+          >
+            {`${aspects5[index]}`}
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
